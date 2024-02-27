4201ecb9df2dc404a40d3b101637f4236e171402
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-02-26 22:39:54 -0500

    Update EjercicioRN5.py

--- EjercicioRN5.py
+++ EjercicioRN5.py
@@ -1,10 +1,11 @@
 class EjercicioRN5:
-  SUMA = 0
-  X = 20
-  SUMA = SUMA + X
-  Y = 40
-  X = X + Y ** 2
-  SUMA = SUMA + X / Y
-  print(f"El valor de la suma es: {SUMA}")
+  def main():
+    SUMA = 0
+    X = 20
+    SUMA = SUMA + X
+    Y = 40
+    X = X + Y ** 2
+    SUMA = SUMA + X / Y
+    print(f"El valor de la suma es: {SUMA}")
 
 EjercicioRN5.main()
