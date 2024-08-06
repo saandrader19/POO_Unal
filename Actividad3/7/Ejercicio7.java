abf4a95193e56cfa70708a344e8b2e9679d63de6
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-08-06 16:55:44 -0500

    Create Ejercicio7.java

---
+++ Actividad3/7/Ejercicio7.java
@@ -0,0 +1,10 @@
+package poo.Ejercicio7;
+
+public class Ejercicio7 {
+    
+    public static void main(String args[]) {
+        Numeros num = new Numeros();
+        
+        num.mayor();
+    }
+}
