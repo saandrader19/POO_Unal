2f8f2d5175a893bc8eff60c2234db6469cb65af4
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-08-06 14:01:44 -0500

    Create Actividad3.java

---
+++ Actividad3/Actividad3.java
@@ -0,0 +1,10 @@
+
+package com.actividad3;
+
+public class Actividad3 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.show();
+    }
+}
