fae1d09174a59c155b06060e522696d81da17808
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-08-06 14:57:00 -0500

    Create Ejercicio18.java

---
+++ Actividad3/18/Ejercicio18.java
@@ -0,0 +1,9 @@
+package com.actividad3;
+
+public class Ejercicio18 {
+
+    public static void main(String[] args) {
+        Ventana18 ventana = new Ventana18();
+        ventana.show();
+    }
+}
