47a77277fffcac67f0a469cc41282ee691d2b00f
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-08-06 20:42:39 -0500

    Create Ejercicio23.java

---
+++ Actividad3/23/Ejercicio23.java
@@ -0,0 +1,9 @@
+package com.actividad3;
+
+public class Ejercicio23 {
+
+    public static void main(String[] args) {
+        Ventana23 ventana = new Ventana23();
+        ventana.show();
+    }
+}
