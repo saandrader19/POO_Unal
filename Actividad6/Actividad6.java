6ec85dc988d645a44c15235bf34a1287eafd6152
saandrader19 <161407425+saandrader19@users.noreply.github.com>
2024-09-24 23:36:08 -0500

    Create Actividad6.java

---
+++ Actividad6/Actividad6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.friends;
+
+/**
+ *
+ * @author Andrade
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        MainForm frm = new MainForm();
+        frm.setVisible(true);
+    }
+}
